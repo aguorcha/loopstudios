bacd2124a414f47877508034b68e8c26de7e041d
Agu Orcha <aguorcha@gmail.com>
2024-06-23 15:51:45 +0200

    Maquetación del header con los hovers terminados

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -9,32 +9,35 @@ defineProps({
 
 <template>
   <header class="relative w-full h-[640px] bg-cover bg-no-repeat flex items-center justify-center">
-    <img class="relative w-screen" src="../assets/images/mobile/image-hero.jpg" alt="Image Hero">
-    <div class="absolute w-full top-0 left-0 mt-10 flex justify-between items-center">
-      <img class="w-44 pl-6" src="../assets/images/logo.svg" alt="Loopstudios Logo">
-      <img class="w-14 h-5 pr-6" src="../assets/images/icon-hamburger.svg" alt="Menu Icon">
-    </div>
-    <div class="absolute w-4/5 p-6 pr-20 border-2 border-white">
-      <h2 class="font-josefin-sans text-4xl font-light leading-none text-white">IMMERSIVE EXPERIENCES THAT DELIVER</h2>
+    <div class="bg-header-image-mobile absolute w-full h-full bg-cover bg-no-repeat sm:hidden"></div>
+    <div class="bg-header-image-desktop absolute w-full h-full bg-cover bg-no-repeat hidden sm:block"></div>
+
+    <div class="header-info-container absolute w-full top-0 left-0 mt-10 grid grid-cols-2 sm:grid-rows-2 sm:grid-cols-2 sm:px-36 sm:py-10 items-center">
+      <div class="col-span-1 ml-8 sm:row-span-1">
+        <img class="logo w-44 sm-6" src="../assets/images/logo.svg" alt="Loopstudios Logo">
+      </div>
+      <div class="col-span-1 flex justify-end md:hidden">
+        <img class="menu-icon w-14 h-5 pr-6" src="../assets/images/icon-hamburger.svg" alt="Menu Icon">
+      </div>
+      
+      <nav class="hidden md:flex justify-end space-x-8 text-white font-alata text-sm md:-mr-20 lg:mr-10">
+        <a href="#" class="nav-link">About</a>
+        <a href="#" class="nav-link">Careers</a>
+        <a href="#" class="nav-link">Events</a>
+        <a href="#" class="nav-link">Products</a>
+        <a href="#" class="nav-link">Support</a>
+      </nav>
+
+      <div class="col-span-2 flex justify-center sm:justify-start sm:col-span-2 sm:row-span-1 mt-[160px] sm:mt-24 sm:ml-6">
+        <div class="title-container absolute w-[88%] p-6 pr-20 border-2 border-white sm:w-[460px]">
+          <h2 class="font-josefin-sans text-4xl font-light leading-none text-white sm:text-5xl">IMMERSIVE EXPERIENCES THAT DELIVER</h2>
+        </div>
+      </div>
+      <div class="col-span-1 md:row-span-1"></div>
     </div>
   </header>
 </template>
 
 <style scoped>
-  /* .title-container {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -60%);
-    width: 80%;
-    padding: 24px;
-    padding-right: 80px;
-    border: 2px solid white;
-  }
-  .title-container h2 {
-    font-size: 2.4rem;
-    font-weight: 300;
-    line-height: 1;
-    color: white;
-  } */
+
 </style>
