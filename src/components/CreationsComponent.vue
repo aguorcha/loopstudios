f71093754ced07a23c41bab02bdd525a13957d60
Agu Orcha <aguorcha@gmail.com>
2024-07-02 13:39:15 +0200

    Maquetación del CreationsComponent

--- src/components/CreationsComponent.vue
+++ src/components/CreationsComponent.vue
@@ -1,65 +1,78 @@
 <template>
   <div class="flex flex-col justify-center items-center mt-16 mb-20 p-6">
-    <h2 class="title text-center text-4xl font-josefin-sans font-light mb-10">OUR CREATIONS</h2>
-    <button class="order-last mt-10 w-1/2 p-2 font-alata tracking-extra border-2 border-black">SEE ALL</button>
+    <div class="md:w-[78%] md:p-6 md:flex md:justify-between md:items-center">
+      <h2 class="title text-center text-4xl font-josefin-sans font-light mb-10">OUR CREATIONS</h2>
+      <button class="hidden md:block mt-10 w-44 p-2 font-alata tracking-extra border-2 border-black md:mt-[-50px] hover:text-white hover:bg-black trasition-colors duration-200 ease-in-out transform">SEE ALL</button>
+    </div>
     
-    <div class="grid grid-cols-1 gap-6">
-      <div class="relative">
-        <img src="../assets/images/mobile/image-deep-earth.jpg" alt="Deep Earth">
-        <div class="absolute top-4 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-56">
-          <h3 class="">DEEP EARTH</h3>
+    <div>
+      <div class="grid grid-cols-1 gap-6 md:grid-cols-4">
+        <div class="relative cursor-pointer hover:opacity-50 transition-opacity duration-200 ease-in-out">
+          <img class="md:hidden" src="../assets/images/mobile/image-deep-earth.jpg" alt="Deep Earth">
+          <img class="hidden md:block" src="../assets/images/desktop/image-deep-earth.jpg" alt="Deep Earth">
+          <div class="absolute bottom-0 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-56">
+            <h3 class="">DEEP EARTH</h3>
+          </div>
         </div>
-      </div>
 
-      <div class="relative">
-        <img src="../assets/images/mobile/image-night-arcade.jpg" alt="Nigth Arcade">
-        <div class="absolute top-4 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-56">
-          <h3 class="">NIGHT ARCADE</h3>
+        <div class="relative cursor-pointer hover:opacity-50 transition-opacity duration-200 ease-in-out">
+          <img class="md:hidden" src="../assets/images/mobile/image-night-arcade.jpg" alt="Night Arcade">
+          <img class="hidden md:block" src="../assets/images/desktop/image-night-arcade.jpg" alt="Night Arcade">
+          <div class="absolute bottom-0 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-56">
+            <h3 class="">NIGHT ARCADE</h3>
+          </div>
         </div>
-      </div>
 
-      <div class="relative">
-        <img src="../assets/images/mobile/image-soccer-team.jpg" alt="Soccer Team VR">
-        <div class="absolute top-4 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-32">
-          <h3 class="">SOCCER TEAM VR</h3>
+        <div class="relative cursor-pointer hover:opacity-50 transition-opacity duration-200 ease-in-out">
+          <img class="md:hidden" src="../assets/images/mobile/image-soccer-team.jpg" alt="Soccer Team VR">
+          <img class="hidden md:block" src="../assets/images/desktop/image-soccer-team.jpg" alt="Soccer Team">
+          <div class="absolute bottom-0 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-32 md:mr-10">
+            <h3 class="">SOCCER TEAM VR</h3>
+          </div>
         </div>
-      </div>
 
-      <div class="relative">
-        <img src="../assets/images/mobile/image-grid.jpg" alt="The Grid">
-        <div class="absolute top-4 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-56">
-          <h3 class="">THE GRID</h3>
+        <div class="relative cursor-pointer hover:opacity-50 transition-opacity duration-200 ease-in-out">
+          <img class="md:hidden" src="../assets/images/mobile/image-grid.jpg" alt="The Grid">
+          <img class="hidden md:block" src="../assets/images/desktop/image-grid.jpg" alt="The Grid">
+          <div class="absolute bottom-0 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-56">
+            <h3 class="">THE GRID</h3>
+          </div>
         </div>
-      </div>
 
-      <div class="relative">
-        <img src="../assets/images/mobile/image-from-above.jpg" alt="From Up Above VR">
-        <div class="absolute top-4 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-32">
-          <h3 class="">FROM UP ABOVE VR</h3>
+        <div class="relative cursor-pointer hover:opacity-50 transition-opacity duration-200 ease-in-out">
+          <img class="md:hidden" src="../assets/images/mobile/image-from-above.jpg" alt="From Up Above VR">
+          <img class="hidden md:block" src="../assets/images/desktop/image-from-above.jpg" alt="From Up Above VR">
+          <div class="absolute bottom-0 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-32 md:mr-12">
+            <h3 class="">FROM UP ABOVE VR</h3>
+          </div>
         </div>
-      </div>
 
-      <div class="relative">
-        <img src="../assets/images/mobile/image-pocket-borealis.jpg" alt="Pocket Borealis">
-        <div class="absolute top-4 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-56">
-          <h3 class="">POCKET BOREALIS</h3>
+        <div class="relative cursor-pointer hover:opacity-50 transition-opacity duration-200 ease-in-out">
+          <img class="md:hidden" src="../assets/images/mobile/image-pocket-borealis.jpg" alt="Pocket Borealis">
+          <img class="hidden md:block" src="../assets/images/desktop/image-pocket-borealis.jpg" alt="Pocket Borealis">
+          <div class="absolute bottom-0 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-56">
+            <h3 class="">POCKET BOREALIS</h3>
+          </div>
         </div>
-      </div>
 
-      <div class="relative">
-        <img src="../assets/images/mobile/image-curiosity.jpg" alt="The Curiosity">
-        <div class="absolute top-4 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-56">
-          <h3 class="">THE CURIOSITY</h3>
+        <div class="relative cursor-pointer hover:opacity-50 transition-opacity duration-200 ease-in-out">
+          <img class="md:hidden" src="../assets/images/mobile/image-curiosity.jpg" alt="The Curiosity">
+          <img class="hidden md:block" src="../assets/images/desktop/image-curiosity.jpg" alt="The Curiosity">
+          <div class="absolute bottom-0 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-56">
+            <h3 class="">THE CURIOSITY</h3>
+          </div>
         </div>
-      </div>
 
-      <div class="relative">
-        <img src="../assets/images/mobile/image-fisheye.jpg" alt="Make It Fisheye">
-        <div class="absolute top-4 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-56">
-          <h3 class="">MAKE IT FISHEYE</h3>
+        <div class="relative cursor-pointer hover:opacity-50 transition-opacity duration-200 ease-in-out">
+          <img class="md:hidden" src="../assets/images/mobile/image-fisheye.jpg" alt="Make It Fisheye">
+          <img class="hidden md:block" src="../assets/images/desktop/image-fisheye.jpg" alt="Make It">
+          <div class="absolute bottom-0 left-0 font-josefin-sans font-light text-white text-2xl p-6 mr-56">
+            <h3 class="">MAKE IT FISHEYE</h3>
+          </div>
         </div>
       </div>
-    </div>      
+    </div>   
+    <button class="md:hidden mt-10 w-44 p-2 font-alata tracking-extra border-2 border-black md:mt-[-50px]">SEE ALL</button>  
   </div>
   
 </template>
